c00fb914d6172a3a8dea751887d7b0f2712f1968
Clawz-123 <pukarbohara9@gmail.com>
2025-11-10 21:10:02 +0545

    Null Operator

--- Week3Workshop/Program.cs
+++ Week3Workshop/Program.cs
@@ -4,11 +4,20 @@
     {
         static void Main(string[] args)
         {
-        Calculator calculator = new Calculator();
-        Console.WriteLine("Addition: " + calculator.Add(5, 3));
-        Console.WriteLine("Subtraction: " + calculator.Subtract(5, 3));
-        Console.WriteLine("Multiplication: " + calculator.Multiply(5, 3));
-        Console.WriteLine("Division: " + calculator.Divide(5, 3));
+            //Calculator calculator = new Calculator();
+            ////OddevenFinder oddevenFinder = new OddevenFinder();  
+            ////Console.WriteLine("Addition: " + calculator.Add(5, 3));
+            ////Console.WriteLine("Subtraction: " + calculator.Subtract(5, 3));
+            ////Console.WriteLine("Multiplication: " + calculator.Multiply(5, 3));
+            ////Console.WriteLine("Division: " + calculator.Divide(5, 3));
+
+            //Console.WriteLine("Odd or Even (5): " + calculator.FindOddEven(5));
+
+            // Call PerformNullChecks from NullOperations
+            NullOperations nullOps = new NullOperations();
+            nullOps.PerformNullChecks();
         }
     }
+
+
 }
