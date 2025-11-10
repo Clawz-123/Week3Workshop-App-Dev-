a93dbd9a492bbb2a6633ca4e6686a6bb566cb621
Clawz-123 <pukarbohara9@gmail.com>
2025-11-10 11:10:29 +0545

    Created a seperate class and done some arithmetic operators

--- Week3Workshop/Program.cs
+++ Week3Workshop/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-         
+        Calculator calculator = new Calculator();
+        Console.WriteLine("Addition: " + calculator.Add(5, 3));
+        Console.WriteLine("Subtraction: " + calculator.Subtract(5, 3));
+        Console.WriteLine("Multiplication: " + calculator.Multiply(5, 3));
+        Console.WriteLine("Division: " + calculator.Divide(5, 3));
         }
     }
 }
